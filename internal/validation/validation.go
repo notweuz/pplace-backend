fc89d91477296120aacea4a09c44206f48595d61
notweuz <weuz.main@gmail.com>
2025-08-14 20:24:17 +0400

    Add regexp validation

--- internal/validation/validation.go
+++ internal/validation/validation.go
@@ -1,15 +1,30 @@
 package validation
 
-import "github.com/go-playground/validator/v10"
+import (
+	"regexp"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type ValidationError struct {
-	Tag   string      `json:"tag"`
+	Error string      `json:"error"`
 	Field string      `json:"field"`
 	Value interface{} `json:"value"`
 }
 
 func ValidateDTO(dto interface{}) []ValidationError {
 	validate := validator.New()
+
+	validate.RegisterValidation("username", func(fl validator.FieldLevel) bool {
+		re := regexp.MustCompile(`^[a-zA-Z0-9_]+$`)
+		return re.MatchString(fl.Field().String())
+	})
+
+	validate.RegisterValidation("password", func(fl validator.FieldLevel) bool {
+		re := regexp.MustCompile(`^[a-zA-Z0-9!@#\$%\^&\*]+$`)
+		return re.MatchString(fl.Field().String())
+	})
+
 	err := validate.Struct(dto)
 	if err != nil {
 		if _, ok := err.(*validator.InvalidValidationError); ok {
@@ -18,7 +33,7 @@ func ValidateDTO(dto interface{}) []ValidationError {
 		var errors []ValidationError
 		for _, e := range err.(validator.ValidationErrors) {
 			errors = append(errors, ValidationError{
-				Tag:   e.Tag(),
+				Error: e.Error(),
 				Field: e.Field(),
 				Value: e.Value(),
 			})
