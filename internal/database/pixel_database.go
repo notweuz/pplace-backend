d02c72c49ae79e0a2e562930df8723bc909bbdfb
notweuz <weuz.main@gmail.com>
2025-10-07 18:27:32 +0400

    Start making pixel database and pixel service

--- internal/database/pixel_database.go
+++ internal/database/pixel_database.go
@@ -1,1 +1,58 @@
 package database
+
+import (
+	"context"
+	"errors"
+	"pplace_backend/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type PixelDatabase struct {
+	db *gorm.DB
+}
+
+func NewPixelDatabase(db *gorm.DB) *PixelDatabase {
+	return &PixelDatabase{db: db}
+}
+
+func (d *PixelDatabase) Create(ctx context.Context, pixel *model.Pixel) (*model.Pixel, error) {
+	result := d.db.WithContext(ctx).Create(pixel)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return pixel, nil
+}
+
+func (d *PixelDatabase) Update(ctx context.Context, pixel *model.Pixel) (*model.Pixel, error) {
+	result := d.db.WithContext(ctx).Save(pixel)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+	return pixel, nil
+}
+
+func (d *PixelDatabase) GetByID(ctx context.Context, id uint) (*model.Pixel, error) {
+	var pixel model.Pixel
+	result := d.db.WithContext(ctx).First(&pixel, id)
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil, nil
+	}
+	return &pixel, result.Error
+}
+
+func (d *PixelDatabase) GetAll(ctx context.Context) ([]model.Pixel, error) {
+	var pixels []model.Pixel
+	return pixels, d.db.WithContext(ctx).Find(&pixels).Error
+}
+
+func (d *PixelDatabase) GetByCoordinates(ctx context.Context, x, y uint) (*model.Pixel, error) {
+	var pixel model.Pixel
+	err := d.db.WithContext(ctx).Where("x = ? AND y = ?", x, y).First(&pixel).Error
+	return &pixel, err
+}
+
+func (d *PixelDatabase) GetAllByUserID(ctx context.Context, userId uint) ([]model.Pixel, error) {
+	var pixels []model.Pixel
+	return pixels, d.db.WithContext(ctx).Where("user_id = ?", userId).Find(&pixels).Error
+}
---
+++ internal/service/pixel_service.go
@@ -0,0 +1,56 @@
+package service
+
+import (
+	"context"
+	"pplace_backend/internal/config"
+	"pplace_backend/internal/database"
+	"pplace_backend/internal/model"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type PixelService struct {
+	database    *database.PixelDatabase
+	config      *config.PPlaceConfig
+	userService *UserService
+}
+
+func NewPixelService(db *database.PixelDatabase, config *config.PPlaceConfig, userService *UserService) *PixelService {
+	return &PixelService{
+		database:    db,
+		config:      config,
+		userService: userService,
+	}
+}
+
+func (s *PixelService) Create(ctx context.Context, user *model.Pixel) (*model.Pixel, error) {
+	return s.database.Create(ctx, user)
+}
+
+func (s *PixelService) Update(ctx context.Context, user *model.Pixel) (*model.Pixel, error) {
+	return s.database.Update(ctx, user)
+}
+
+func (s *PixelService) GetByID(ctx context.Context, id uint) (*model.Pixel, error) {
+	return s.database.GetByID(ctx, id)
+}
+
+func (s *PixelService) GetAll(ctx context.Context) ([]model.Pixel, error) {
+	return s.database.GetAll(ctx)
+}
+
+func (s *PixelService) GetByCoordinates(ctx context.Context, x, y uint) (*model.Pixel, error) {
+	return s.database.GetByCoordinates(ctx, x, y)
+}
+
+func (s *PixelService) GetAllByUser(ctx context.Context, userId uint) ([]model.Pixel, error) {
+	return s.database.GetAllByUserID(ctx, userId)
+}
+
+func (s *PixelService) GetAllByUserSelf(c *fiber.Ctx, ctx context.Context) ([]model.Pixel, error) {
+	user, err := s.userService.GetSelfInfo(c)
+	if err != nil {
+		return nil, err
+	}
+	return s.database.GetAllByUserID(ctx, user.ID)
+}
