441daa1716b9b61364512faa7b9f8d95e206385c
notweuz <weuz.main@gmail.com>
2025-10-08 01:33:27 +0400

    Enhance pixel creation and update error handling to check for nil responses

--- internal/handler/pixel.go
+++ internal/handler/pixel.go
@@ -43,7 +43,7 @@ func (h *PixelHandler) Create(c *fiber.Ctx) error {
 
 	pixel := model.NewPixel(0, pixelCreateDto.X, pixelCreateDto.Y, pixelCreateDto.Color)
 	createdPixel, err := h.service.Create(c, c.Context(), pixel)
-	if err != nil {
+	if err != nil || createdPixel == nil {
 		return err
 	}
 
@@ -78,7 +78,7 @@ func (h *PixelHandler) Update(c *fiber.Ctx) error {
 
 	pixel := model.NewPixel(uint(id), 0, 0, pixelUpdateDto.Color)
 	updatedPixel, err := h.service.Update(c, c.Context(), pixel)
-	if err != nil {
+	if err != nil || updatedPixel == nil {
 		return err
 	}
 
