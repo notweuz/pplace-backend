218a4d5919bffe7e791b7ee5b1b1712212b07289
notweuz <weuz.main@gmail.com>
2025-10-07 13:00:34 +0400

    Change .Err() to .Info() in http logging (mistyped oops)

--- internal/layer/middleware/http_logging.go
+++ internal/layer/middleware/http_logging.go
@@ -21,9 +21,9 @@ func LoggingMiddleware() fiber.Handler {
 		if err != nil {
 			log.Error().Err(err).Msgf("HTTP %s %s failed after %v", c.Method(), c.Path(), duration)
 		} else if status >= 400 {
-			log.Error().Msgf("HTTP %s %s completed with status %d in %v", c.Method(), c.Path(), status, duration)
+			log.Info().Msgf("HTTP %s %s completed with status %d in %v", c.Method(), c.Path(), status, duration)
 		} else {
-			log.Error().Msgf("HTTP %s %s completed with status %d in %v", c.Method(), c.Path(), status, duration)
+			log.Info().Msgf("HTTP %s %s completed with status %d in %v", c.Method(), c.Path(), status, duration)
 		}
 
 		return err
