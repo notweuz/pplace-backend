c1ee1c647be48f893dc4ec2236799859d8b8def8
notweuz <weuz.main@gmail.com>
2025-10-07 20:36:16 +0400

    Add pixel_handler.go errors handling

--- internal/handler/pixel_handler.go
+++ internal/handler/pixel_handler.go
@@ -20,17 +20,19 @@ func NewPixelHandler(service *service.PixelService) *PixelHandler {
 func (h *PixelHandler) Create(c *fiber.Ctx) error {
 	var pixelCreateDto request.PlacePixelDto
 	if err := c.BodyParser(&pixelCreateDto); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Invalid request body",
-		})
+		return c.Status(fiber.StatusBadRequest).JSON(
+			model.NewHttpError(
+				fiber.StatusBadRequest,
+				"Invalid request body",
+				[]string{err.Error()},
+			),
+		)
 	}
 
 	pixel := model.NewPixel(0, pixelCreateDto.X, pixelCreateDto.Y, pixelCreateDto.Color)
 	createdPixel, err := h.service.Create(c, c.Context(), pixel)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Failed to create pixel",
-		})
+		return h.handlePixelError(c, err)
 	}
 
 	authorDto := response.NewUserShortDto(createdPixel.UserID, createdPixel.User.Username)
@@ -41,26 +43,38 @@ func (h *PixelHandler) Create(c *fiber.Ctx) error {
 func (h *PixelHandler) Update(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Invalid pixel ID",
-		})
+		return c.Status(fiber.StatusBadRequest).JSON(
+			model.NewHttpError(fiber.StatusBadRequest, "Invalid pixel ID", []string{err.Error()}),
+		)
 	}
 
 	var pixelUpdateDto request.UpdatePixelDto
 	if err := c.BodyParser(&pixelUpdateDto); err != nil {
-		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": "Invalid request body",
-		})
+		return c.Status(fiber.StatusBadRequest).JSON(
+			model.NewHttpError(fiber.StatusBadRequest, "Invalid request body", []string{err.Error()}),
+		)
 	}
 
 	pixel := model.NewPixel(uint(id), 0, 0, pixelUpdateDto.Color)
 	updatedPixel, err := h.service.Update(c, c.Context(), pixel)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "Failed to update pixel",
-		})
+		return h.handlePixelError(c, err)
 	}
+
 	authorDto := response.NewUserShortDto(updatedPixel.UserID, updatedPixel.User.Username)
 	pixelDto := response.NewPixelDto(updatedPixel.ID, updatedPixel.X, updatedPixel.Y, updatedPixel.Color, *authorDto)
 	return c.Status(fiber.StatusOK).JSON(pixelDto)
+}
+
+func (h *PixelHandler) handlePixelError(c *fiber.Ctx, err error) error {
+	errMsg := err.Error()
+
+	switch {
+	case errMsg == "pixel not found":
+		return c.Status(fiber.StatusNotFound).JSON(model.NewHttpError(fiber.StatusNotFound, errMsg, []string{errMsg}))
+	case errMsg == "pixel already exists":
+		return c.Status(fiber.StatusConflict).JSON(model.NewHttpError(fiber.StatusConflict, errMsg, []string{errMsg}))
+	default:
+		return c.Status(fiber.StatusInternalServerError).JSON(model.NewHttpError(fiber.StatusInternalServerError, "Failed to process pixel", []string{errMsg}))
+	}
 }
