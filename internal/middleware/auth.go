5765ad9d01a1c9f208eaa819cea3fde05c59bdf4
notweuz <weuz.main@gmail.com>
2025-10-08 02:31:20 +0400

    Update outdated logging in auth middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"pplace_backend/internal/model/dto/response"
 	"pplace_backend/internal/service"
 
 	"github.com/gofiber/fiber/v2"
@@ -10,17 +11,12 @@ func AuthMiddleware(userService *service.UserService) fiber.Handler {
 	return func(c *fiber.Ctx) error {
 		tokenString, err := userService.ExtractToken(c)
 		if err != nil {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"error": "Invalid authorization header",
-			})
+			return response.NewHttpError(fiber.StatusUnauthorized, "Authorization token missing", []string{err.Error()})
 		}
 
 		user, err := userService.ParseAndValidateToken(tokenString)
 		if err != nil {
-			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"error":   "Invalid or expired token",
-				"details": err.Error(),
-			})
+			return response.NewHttpError(fiber.StatusUnauthorized, "Invalid or expired token", []string{err.Error()})
 		}
 
 		c.Locals("user", user)
